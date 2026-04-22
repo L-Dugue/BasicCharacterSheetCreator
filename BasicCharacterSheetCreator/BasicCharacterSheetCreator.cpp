80c24e62796bb2cf76cc7ed8a5f2ea525d0989c7
L-Dugue <ldugue2007@outlook.com>
2026-04-22 14:54:33 -0400

    [ADD] Added Project Files

---
+++ BasicCharacterSheetCreator/BasicCharacterSheetCreator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+
+	cout << "\033[" << 32 << "m"; // Changes text color of the console to Green
+
+	cout << "Hello, World!\n";
+
+	return 0;
+}
