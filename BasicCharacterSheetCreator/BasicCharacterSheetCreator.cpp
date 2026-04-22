bd1e2cb7b39e5b22b391cc578bb6a00f3c2f3d02
L-Dugue <ldugue2007@outlook.com>
2026-04-22 15:12:31 -0400

    [ADD] Added DefineCharacter method

--- BasicCharacterSheetCreator/BasicCharacterSheetCreator.cpp
+++ BasicCharacterSheetCreator/BasicCharacterSheetCreator.cpp
@@ -1,13 +1,27 @@
 #include <iostream>
-
+#include <windows.h>
 using namespace std;
+
+// Variables
+string characterName;
+
+void DefineCharacter(); // Declaration of Methods
 
 int main() {
 	
 
 	cout << "\033[" << 32 << "m"; // Changes text color of the console to Green
-
-	cout << "Hello, World!\n";
+	LPCWSTR consoleName = L"Avro's Basic Character Creator Sheet";
+	SetConsoleTitle(consoleName);
+	cout << "---------------------------------------------------\n";
+	DefineCharacter();
 
 	return 0;
+}
+
+// Initialization of Methods
+void DefineCharacter() {
+	cout << "What is your Character's Name?\n" << "Enter Character's Name:";
+	cin >> characterName;
+	cout << "Character's name is: " << characterName;
 }
