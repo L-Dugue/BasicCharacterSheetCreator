e0aa1c64f3b4fd65052ac90292375f8c8e308b45
L-Dugue <ldugue2007@outlook.com>
2026-04-22 21:14:42 -0400

    [ADD] Added CharacterTemplate class, made methods for defining name and stats, defined arrays and enums for char values

--- BasicCharacterSheetCreator/BasicCharacterSheetCreator.cpp
+++ BasicCharacterSheetCreator/BasicCharacterSheetCreator.cpp
@@ -1,27 +1,68 @@
 #include <iostream>
-#include <windows.h>
+#include <format>
 using namespace std;
 
-// Variables
-string characterName;
+class CharacterTemplate {
 
-void DefineCharacter(); // Declaration of Methods
+	// Basic Private Fields
+private:
+		string _characterName;
+		
+
+	// Private Arrays and Enums
+private:
+		enum basicStatsEnums {
+			SPEED,
+			STAMINA,
+			STRENGTH,
+			AGILITY,
+			SEXINESS,
+		};
+		int basicStats[static_cast<int>(basicStatsEnums::SEXINESS) + 1] = { 0, 0, 0, 0, 0};
+		string basicStatsNames[static_cast<int>(basicStatsEnums::SEXINESS) + 1] = {"Speed", "Stamina", "Strength", "Agility", "Sexiness"};
+		
+
+		// Private Methods
+		void DefineName() {
+			
+			cout << "\nWhat is your Character's Name?\n" << "Enter Character's Name:";
+			cin >> _characterName;
+		}
+
+		void DefineStats() {
+			cout << format("\nDefine {} stats!", _characterName) << endl;
+			cout << "----------------------------------------------------------" << endl;
+			for (int i = 0; i < static_cast<int>(basicStatsEnums::SEXINESS); i++) {
+
+				cout << format("\n{}'s {} value: ", _characterName, basicStatsNames[i]);
+				cin >> basicStats[i];
+				cout << "\n";
+
+			}
+
+			cout << "Stats Defined!" << endl << "----------------------------------------------------------";
+		}
+
+
+public:
+		void StartCharacterCreation() {
+			DefineName();
+			DefineStats();
+		}
+
+};
+
+
 
 int main() {
 	
+	CharacterTemplate characterTemp;
 
 	cout << "\033[" << 32 << "m"; // Changes text color of the console to Green
-	LPCWSTR consoleName = L"Avro's Basic Character Creator Sheet";
-	SetConsoleTitle(consoleName);
-	cout << "---------------------------------------------------\n";
-	DefineCharacter();
-
+	characterTemp.StartCharacterCreation();
 	return 0;
 }
 
+
+
 // Initialization of Methods
-void DefineCharacter() {
-	cout << "What is your Character's Name?\n" << "Enter Character's Name:";
-	cin >> characterName;
-	cout << "Character's name is: " << characterName;
-}
